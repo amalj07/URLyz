4aa3dfdcf176e36a6975ab2e52cb893f12040383
Amal Jose <amalavirappatt@yahoo.com>
2020-07-23 17:14:53 +0530

    create url model

---
+++ server/models/Url.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+
+const urlSchema = new mongoose.Schema({
+    urlCode = {
+        type: String,
+        reqiured: true
+    },
+    longUrl: {
+        type: String,
+        required: true
+    },
+    shortUrl: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: String,
+        default: Date.now
+    }
+})
+
+const urlSchemaModel = mongoose.model('urls', urlSchema)
+
+module.exports = urlSchemaModel
