a562cbad11abd1f85112a2d7f9fe70223a218dda
Amal Jose <amalavirappatt@yahoo.com>
2020-07-23 15:01:40 +0530

    feat(home):create basic home page

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,5 +1,32 @@
 <template>
-  <p>This is home page</p>
+    <div class="home">
+        <v-container>
+            <div class="my-16">
+                <p class="text-center text-h3 pt-5">myURL</p>
+                <p class="text-center text-h5 pb-5">Create your own links...</p>
+            </div>
+            <div>
+                <v-row justify="center">
+                   <v-col cols="2" md="6">
+                        <v-text-field
+                            flat
+                            label="Solo"
+                            placeholder="Place your link here..."
+                            solo
+                            height="60"
+                            outlined
+                            color="deep-purple lighten-1"
+                        ></v-text-field>
+                    </v-col>
+                    <v-col cols="1" md="1">
+                        <div>
+                            <v-btn flat height="57" color="deep-purple lighten-1 white--text">Shorten</v-btn>
+                        </div>
+                    </v-col>
+                </v-row>
+            </div>
+        </v-container>
+    </div>
 </template>
 
 <script>
