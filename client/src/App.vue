b03c31db332cdbd7738d61f5756efbbd158352c0
Amal Jose <amalavirappatt@yahoo.com>
2020-07-23 10:36:27 +0530

    feat(navbar):create navbar component

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,25 +1,18 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <v-toolbar-title class="white--text">
-        <span class="font-weight-light">my</span>
-        <span>URL</span>
-      </v-toolbar-title>
-    </v-app-bar>
-
+    <Navbar />
     <v-main>
     </v-main>
   </v-app>
 </template>
 
 <script>
+import Navbar from '@/components/Navbar'
 export default {
   name: 'App',
-
+  components: {
+    Navbar
+  },
   data: () => ({
     //
   }),
---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,28 @@
+<template>
+  <nav>
+      <v-app-bar flat app class="deep-purple lighten-1">
+          <v-toolbar-title class="white--text">
+              <span class="font-weight-light">my</span>
+              <span>URL</span>
+          </v-toolbar-title>
+
+        <v-spacer></v-spacer>
+        <v-btn depressed class="deep-purple lighten-1 white--text">
+        <span>Signin</span>
+        </v-btn>
+        <v-btn rounded class="white purple--text">
+        <span>Register</span>
+        </v-btn>
+      </v-app-bar>
+  </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
