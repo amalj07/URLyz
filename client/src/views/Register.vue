e0962dffe774e0cf45064f2a346e009769c5bf1f
Amal Jose <amalavirappatt@yahoo.com>
2020-07-26 22:29:14 +0530

    add register form validation

--- client/src/views/Register.vue
+++ client/src/views/Register.vue
@@ -5,7 +5,7 @@
                 <p class="text-center text-h4">Create new account</p>
             </div> -->
             <v-row justify="center">
-                <v-form class="mt-10">
+                <v-form class="mt-10" ref="registerForm">
                     <v-card
                         outlined
                         min-width="450">
@@ -15,18 +15,24 @@
                         <div class="px-10 pt-7">
                             <v-text-field
                                 label="Name"
+                                :rules="inputRules"
+                                type="text"
                                 outlined
                                 dense
                                 v-model="name"
                             ></v-text-field>
                             <v-text-field
                                 label="Email"
+                                :rules="inputRules.concat(emailRules)"
+                                type="email"
                                 outlined
                                 dense
+                                validate-on-blur
                                 v-model="email"
                             ></v-text-field>
                             <v-text-field
                                 label="Password"
+                                :rules="inputRules"
                                 type="password"
                                 outlined
                                 dense
@@ -34,9 +40,11 @@
                             ></v-text-field>
                             <v-text-field
                                 label="Confirm password"
+                                :rules="inputRules.concat(confirmPasswordRule)"
                                 type="password"
                                 outlined
                                 dense
+                                validate-on-blur
                                 v-model="confirmPassword"
                             ></v-text-field>
                         </div>
@@ -63,21 +71,35 @@ export default {
             name: '',
             email: '',
             password: '',
-            confirmPassword: ''
+            confirmPassword: '',
+            inputRules: [
+                value => value.length > 0 || 'required',
+            ],
+            emailRules: [ 
+                v => !v || /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) || 'E-mail must be valid'
+            ]
         }
     },
     methods: {
         registerUser() {
-            let url= 'http://localhost:5000/api/user/user_register'
-            this.$http.post(url, {
-                name: this.name,
-                email: this.email,
-                password: this.password
-            }).then(response => {
-                console.log(response.data)
-            }).catch(error => {
-                console.log(error)
-            })
+            if(this.$refs.registerForm.validate()) {
+                this.$refs.registerForm.validate()
+                let url= 'http://localhost:5000/api/user/user_register'
+                this.$http.post(url, {
+                    name: this.name,
+                    email: this.email,
+                    password: this.password
+                }).then(response => {
+                    console.log(response.data)
+                }).catch(error => {
+                    console.log(error)
+                })
+            }     
+        }
+    },
+    computed: {
+        confirmPasswordRule() {
+            return () => (this.password === this.confirmPassword) || 'Passwords doesn\'t match'
         }
     }
 }
