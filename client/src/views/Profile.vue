67be079e251acc87b0ff611ddb794f3d19bdf10b
Amal Jose <amalavirappatt@yahoo.com>
2020-08-23 13:51:55 +0530

    update profile page

--- client/src/views/Profile.vue
+++ client/src/views/Profile.vue
@@ -1,28 +1,65 @@
 <template>
     <div>
         <NavDrawer />
-        <div  class="mt-10 ml-8">
+        <div  class="mt-3 ml-8">
             <v-container>
-                <h1 class="mb-10 text-decoration-underline grey--text text--darken-2 font-italic">My Profile</h1>
+                <h1 class="mb-7 text-decoration-underline grey--text text--darken-2 font-italic">My Profile</h1>
                 <div>
-                    <v-card outlined max-width="900">
+                    <v-card class="mb-2" outlined max-width="900">
                         <div class="ml-10">
                             <h3 class="my-3">Name:</h3>
-                            <v-text-field
-                                class="textfeildwidth ml-5 m"
-                                dense
-                                disabled
-                                outlined
-                                v-model="name"  
-                            ></v-text-field>
+                            <v-row>
+                                <v-text-field
+                                    class="textfeildwidth ml-5 m"
+                                    dense
+                                    disabled
+                                    outlined
+                                    v-model="name"  
+                                ></v-text-field>
+                                <v-btn class="ml-2 mt-1 blue--text text--darken-2" text small>
+                                    <v-icon small>edit</v-icon>
+                                    <span>Edit</span>
+                                </v-btn>
+                            </v-row>
                             <h3 class="mb-3">Email:</h3>
-                            <v-text-field
-                                class="textfeildwidth ml-5"
-                                dense
-                                disabled
-                                outlined
-                                v-model="email"  
-                            ></v-text-field>
+                            <v-row>
+                                <v-text-field
+                                    class="textfeildwidth ml-5"
+                                    dense
+                                    disabled
+                                    outlined
+                                    v-model="email"  
+                                ></v-text-field>
+                                <v-btn class="ml-2 mt-1 blue--text text--darken-2" text small>
+                                    <v-icon small>edit</v-icon>
+                                    <span>Edit</span>
+                                </v-btn>
+                            </v-row>
+                        </div>
+                    </v-card>
+                    <v-card class="mt-7" outlined max-width="900">
+                        <div class="ml-10">
+                            <v-col class="mt-3">
+                                <v-text-field
+                                    class="textfeildwidth"
+                                    dense
+                                    outlined
+                                    type="password"
+                                    placeholder="Enter your current password"
+                                    v-model="password"  
+                                ></v-text-field>
+                                <v-btn
+                                    class="blue white--text mt-n2">
+                                    Change Password
+                                </v-btn>
+                            </v-col>
+                            <v-col class="mt-4">
+                                <v-btn
+                                    class="red white--text">
+                                    Delete Account
+                                </v-btn>
+                                <p class="caption mt-2 grey--text text--darken-1">Deleting your account removes all your account data.</p>
+                            </v-col>
                         </div>
                     </v-card>
                 </div>
