4307caf40b7f41f3179d6b5d223c88b016081b64
Amal Jose <amalavirappatt@yahoo.com>
2020-09-19 22:14:47 +0530

    wip: extension panel

--- client/src/views/Dashboard.vue
+++ client/src/views/Dashboard.vue
@@ -80,54 +80,84 @@
           <h1 class="mb-10 text-decoration-underline grey--text text--darken-2 font-italic">My URLs</h1>
           <!-- <div v-for="(url, index) in urls" :key="url.urlCode"> -->
             <!-- <v-card outlined min-height="70" max-width="800" class="mb-4 py-2"> -->
-              <v-expansion-panels popout>
+              <v-expansion-panels popout tile hover>
                 <v-expansion-panel
+                  class="mb-3"
                   v-for="(url,index) in urls"
                   :key="index"
                 >
               <center>
-                    <div class="text-left">
-                      <v-expansion-panel-header>
-                      <a :href="url.shortUrl" target="_blank" class="grey--text text--darken-3">{{ url.shortUrl }}</a>
-                      </v-expansion-panel-header>
-                    </div>
-                     <v-expansion-panel-content>
-                       <div class="text-left">
-                       <span>Status: </span>
-                      <v-chip
-                        label
-                        outlined
-                        small
-                        :text-color="getColor(url.status)"
-                        :color="getColor(url.status)"
-                        class="my-3 size">
-                        <span>{{ url.status }}</span>
-                      </v-chip>
-                      </div>
-                    <div class="text-left">
-                      <v-btn
-                        outlined
-                        shaped
-                        small
-                        min-width=83
-                        class="my-3"
-                        color="blue darken-2"
-                        @click="updateLink(url, index)">
-                        <span v-if="url.status === 'active'">Disable</span>
-                        <span v-else>Enable</span>
-                      </v-btn>
-                    </div>
-                    <div class="text-left">
-                      <v-btn
-                        text
-                        :ripple=false
-                        shaped
-                        class="del_btn my-2 white blue--text text--darken-2">
-                        <v-icon @click="deleteUrl(url, index)">delete</v-icon>
-                      </v-btn>
-                    </div>
-                   </v-expansion-panel-content>
+                <v-expansion-panel-header>
+                  <div class="text-left">
+                    <a :href="url.shortUrl" target="_blank" class="grey--text text--darken-3">{{ url.shortUrl }}</a>
+                  </div>
+                  <!-- <div class="">
+                    <v-chip
+                      label
+                      outlined
+                      small
+                      :text-color="getColor(url.status)"
+                      :color="getColor(url.status)"
+                      class="my-3 size">
+                      <span>{{ url.status }}</span>
+                    </v-chip>
+                  </div> -->
+                  <!-- <div class="">
+                    <v-btn
+                      outlined
+                      shaped
+                      small
+                      min-width=83
+                      class="my-3"
+                      color="blue darken-2"
+                      @click="updateLink(url, index)">
+                      <span v-if="url.status === 'active'">Disable</span>
+                      <span v-else>Enable</span>
+                    </v-btn>
+                  </div> -->
+                  <!-- <div class="">
+                    <v-btn
+                      text
+                      :ripple=false
+                      shaped
+                      class="del_btn my-2 white blue--text text--darken-2">
+                      <v-icon @click="deleteUrl(url, index)">delete</v-icon>
+                    </v-btn>
+                  </div> -->
+                </v-expansion-panel-header>
               </center>
+                <v-expansion-panel-content>
+                  <v-chip
+                      label
+                      outlined
+                      small
+                      :text-color="getColor(url.status)"
+                      :color="getColor(url.status)"
+                      class="my-3 size">
+                      <span>{{ url.status }}</span>
+                    </v-chip>
+                    <v-btn
+                      outlined
+                      shaped
+                      small
+                      min-width=83
+                      class="my-3"
+                      color="blue darken-2"
+                      @click="updateLink(url, index)">
+                      <span v-if="url.status === 'active'">Disable</span>
+                      <span v-else>Enable</span>
+                    </v-btn>
+                  <p>Total visits: {{ url.visits }} </p>
+                  <p>Original URL: {{ url.longUrl }} </p>
+                  <v-btn
+                      text
+                      :ripple=false
+                      shaped
+                      class="del_btn my-2 white blue--text text--darken-2">
+                      <!-- <v-icon @click="deleteUrl(url, index)">delete</v-icon> -->
+                      <span>Delete URL</span>
+                    </v-btn>
+                </v-expansion-panel-content>
               </v-expansion-panel>
               </v-expansion-panels>
             <!-- </v-card> -->
