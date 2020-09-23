5c231ff3d57b0c18cf4fd8ec8c89904cf96d6862
Amal Jose <amalavirappatt@yahoo.com>
2020-09-23 23:25:53 +0530

    fix 404 error page not shown

--- client/src/views/NotFoundPage.vue
+++ client/src/views/NotFoundPage.vue
@@ -5,7 +5,7 @@
             <img src="../assets/404.svg" alt="">
             <p class="my-9 text-h4 grey--text text--darken-2">The page you're looking for cannot be found!</p>
             <!-- <a class="mt-10 blue--text text--darken-2 text-h5" :href="`${this.$serverURLI}`"> -->
-            <a class="mt-10 blue--text text--darken-2 text-h5" :href="window.location.href = process.env.VUE_APP_CLIENT_URL">
+            <a class="mt-10 blue--text text--darken-2 text-h5" :href="this.url">
                 <span>Short a looooooong URL</span>
                 <v-icon class="ml-2 blue--text text--darken-2 arrow">arrow_forward</v-icon>
             </a>
@@ -16,7 +16,11 @@
 
 <script>
 export default {
-
+    data() {
+        return {
+            url: process.env.VUE_APP_CLIENT_URL
+        }
+    }
 }
 </script>
 
